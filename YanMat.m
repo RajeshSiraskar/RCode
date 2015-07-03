e3692b1af02271e4d8a12eb65af098b72b4acf8c
RajeshSiraskar <rajesh.siraskar@kpit.com>
2015-07-03 17:55:14 +0530

    Create YanMat.m

---
+++ YanMat.m
@@ -0,0 +1,72 @@
+%-----------------------------------------------------------
+% Yanmar Project
+% Date:     03-July-2015
+% Author:   Rajesh Siraskar
+%-----------------------------------------------------------
+% Event at: 16-May-2010 23:11:42 i.e. Record 85,2110
+% Important Features: 10, 21 and 24
+% Data frequency: 5 secs.
+% Benchmark: 4 mins = 4*60 = 240 secs. i.e. 48 records
+%-----------------------------------------------------------
+% Idea: Regression on normalized ALL vars.
+% 4	5	10	18	19	20	21	22	23	24	25	27	28	29	30	31
+%-----------------------------------------------------------
+
+clc;
+
+fprintf('\n-----------------------------------------------------------\n');
+fprintf('\n              YANMAR PROJECT\n');
+fprintf('\n              Regression on normalized ALL vars.\n');
+fprintf('\n 4	5	10	18	19	20	21	22	23	24	25	27	28	29	30	31\n');
+fprintf('\n-----------------------------------------------------------\n');
+
+% # Define
+LOADDATA = true;
+rEvent = 852000;  % Actual recorded event: 852110
+windowPre = 50000;
+windowPost = 10;
+windowBM = windowPre - (4*60/5); % Benchmark, 4 mins
+nVars = 16; 
+
+iFeatures = [4	5	10	18	19	20	21	22	23	24	25	27	28	29	30	31];
+
+% Create matrix for just selected features
+nRecords = (windowPre + windowPost + 1);
+YData = zeros(nRecords, length(iFeatures));
+
+if (LOADDATA)
+    fprintf('\nLoad data...');
+    load ('YanmarData.mat');
+    fprintf('\nCut data...');
+    
+    for i = 1:nVars
+        YData(:,i) = Data1((rEvent-windowPre):(rEvent+windowPost), iFeatures(i));
+    end
+    
+    fprintf('\nClear all other variables...\n\n');
+    clearvars variables Data1 Data2 Data3 AllTimeStamp;
+    LOADDATA = false;
+end
+
+% Feature normalize variables to scale
+
+for i = 1:nVars
+    % Normalize column 1
+    mu = mean(YData(:,i));
+    stddev = std(YData(:,i));
+    mu = repmat(mu, [length(YData) 1]);
+    stddev = repmat(stddev, [length(YData) 1]);
+
+    YData (:,i) = (YData(:,i) - mu)./stddev;
+end
+
+
+for i = 1:nVars
+    fprintf('\nPlot %s\n', char(Ship.VarNames(iFeatures(i))));
+    pause();
+    plot(YData(:,i));
+    hold on;
+    line ([windowPre windowPre], [-2 4], 'LineWidth', 1, 'LineStyle', '-.', 'Color', [0.2 0.4 1.0])
+    line ([windowBM windowBM],   [-2 4], 'LineWidth', 1, 'LineStyle', '-.', 'Color', [0.2 0.4 1.0])
+   
+end
